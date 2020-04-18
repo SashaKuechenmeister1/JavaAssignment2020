2e320fdca6640b2119f902c262602c57812ba521
Sasha2.0 <Sasha2.0@Sasha-PC>
2020-04-18 17:29:24 +0100

    Updated Control

--- JavaAssignment/src/com/Assignment/Control.java
+++ JavaAssignment/src/com/Assignment/Control.java
@@ -1,13 +1,17 @@
 package com.Assignment;
 
+/*
+Control CLASS
+ 
+Function: 
+- Allows the GUI to be displayed
+ */
+
 public class Control
 {
 	public static void main(String[] args)
 	{
 		CovidGUI covidgui = new CovidGUI();
-		
-		MachineLearning ML = new MachineLearning("MLdata.csv");
-		System.out.println("Chance of having covid19: "+ML.predict(new Input("hot",true,true,true,false))+"%");
 	}
 }
 
